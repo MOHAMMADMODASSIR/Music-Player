8a7a7be1ef862c7f14f11e9dcbbb29926d5575d2
MOHAMAMDMODASSIR <emailtomdmodassir3@gmail.com>
2022-06-11 15:56:48 +0530

    First Commit

---
+++ js/music-list.js
@@ -0,0 +1,55 @@
+// To add more song, just copy the following code and paste inside the array
+
+//   {
+//     name: "Here is the music name",
+//     artist: "Here is the artist name",
+//     img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+//     src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+//   }
+
+//paste it inside the array as more as you want music then you don't need to do any other thing
+
+let allMusic = [{
+        name: "Aye Khuda - Murder2",
+        artist: "Kshitij Tarey, Saim Bhat, Mithoon",
+        img: "music-1",
+        src: "music-1"
+    },
+    {
+        name: "Kaise Hua - Kabir Singh",
+        artist: "Vishal Mishra",
+        img: "music-2",
+        src: "music-2"
+    },
+    {
+        name: "Awargi - Love Games",
+        artist: "Sangeet, Rasika Shekar",
+        img: "music-3",
+        src: "music-3"
+    },
+    {
+        name: "Closer - The Chainsmokers",
+        artist: "Halsey",
+        img: "music-4",
+        src: "music-4"
+    },
+    {
+        name: "Jannat - Sufna",
+        artist: "B Praak",
+        img: "music-5",
+        src: "music-5"
+    },
+    {
+        name: "Zaroori Tha - Back 2 Love",
+        artist: "Rahat Fateh Ali Khan",
+        img: "music-6",
+        src: "music-6"
+    },
+    // like this paste it and remember to give comma after ending of this bracket }
+    // {
+    //   name: "Here is the music name",
+    //   artist: "Here is the artist name",
+    //   img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+    //   src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+    // }
+];
